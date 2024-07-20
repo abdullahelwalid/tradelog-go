daad534e80a14e73a5bad1a23e0df8b511e8cff2
abdullahelwalid <abdallah.elmansoor@hotmail.com>
2024-07-20 16:01:31 +0800

    removing env package

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -5,17 +5,17 @@ import (
 	//"github.com/abdullahelwalid/tradelog-go/pkg/utils"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/log"
-	"github.com/joho/godotenv"
+	//"github.com/joho/godotenv"
 )
 
 
 func main() {
 	app := fiber.New()
 	routes.MainRouter(app)
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	//err := godotenv.Load()
+	//if err != nil {
+	//	log.Fatal("Error loading .env file")
+	//}
 	//init DB
 	//utils.InitDB()
 	log.Info("Server running on port 8000")
