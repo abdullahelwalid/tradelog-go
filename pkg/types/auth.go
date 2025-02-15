aec33cfd8af32761cd3e83f1774939b41b8bf235
Abdullah Elwalid <abdullah@ip-192-168-100-71.ap-southeast-1.compute.internal>
2025-02-15 13:19:14 +0800

    cookies type

---
+++ pkg/types/auth.go
@@ -0,0 +1,9 @@
+package types
+
+type AuthCookies struct {
+		RefreshToken string `json:"refreshToken"`
+		AccessToken string	`json:"accessToken"`
+		Email string	`json:"email"`
+		Username string	`json:"Username"`
+	}
+
