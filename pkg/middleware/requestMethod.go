369d1fe003619fd982e4f72f3d60ae0c4ba80fe3
Abdullah Elwalid <abdullah@ip-192-168-100-71.ap-southeast-1.compute.internal>
2025-02-15 13:17:57 +0800

    CORS Headers

--- pkg/middleware/requestMethod.go
+++ pkg/middleware/requestMethod.go
@@ -8,6 +8,16 @@ import (
 
 func MethodCheckMiddleware(next http.Handler, allowedMethods []string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+
 		// Check if the request method is allowed
 		for _, method := range allowedMethods {
 			if r.Method == method {
@@ -16,7 +26,7 @@ func MethodCheckMiddleware(next http.Handler, allowedMethods []string) http.Hand
 				return
 			}
 		}
-
+		
 		// If method does not match any of the allowed methods, return 405 Method Not Allowed
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 	}
